535558908dcc94fc5d505d66ea466d9b8412f734
Tehzeeb Shahzadi <tehzeebshahzadi396@gmail.com>
2026-08-21 17:41:41 +0500

    feat/day3

--- app.js
+++ app.js
@@ -8,3 +8,45 @@ let b=5;
 
 console.log(a+b);
 console.log(a/b);
+console.log(a*b);
+
+console.log(10>5);
+console.log(10<5);
+console.log(10===10);
+console.log(10===5);
+console.log(10==="5");
+console.log(10 !==5);
+console.log(10 !==10);
+
+//logical operators
+
+let age =20;
+
+console.log(age >28 && age <60);
+console.log(age <10 || age >18);
+console.log(!(age >18));
+
+
+let number = -5;
+
+if (number === -5) {
+    console.log("Number is -5");
+}
+else{
+    console.log("Error");
+}
+// for loop
+
+for(let i=1; i<=5; i++){
+    console.log(i);
+}
+for (let i=1; i<=5; i++){
+    let marks =i*20;
+    if(marks>=80){
+        console.log("Grade A" );
+    }else if (marks >=60){
+        console.log("Grade B");
+    }else{
+        console.log("Grade F");
+    }
+}
