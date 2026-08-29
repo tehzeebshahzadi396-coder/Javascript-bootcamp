e0fce51a5f59abe6ece57542034a34363b0ea310
Tehzeeb Shahzadi <tehzeebshahzadi396@gmail.com>
2026-08-29 18:57:01 +0500

    feat-day4

--- app.js
+++ app.js
@@ -1,52 +1,68 @@
-console.log("Hello world");
-console.log(5+5);
+// console.log("Hello world");
+// console.log(5+5);
 
-console.log(5-5);
+// console.log(5-5);
 
-let a=10;
-let b=5;
+// let a=10;
+// let b=5;
 
-console.log(a+b);
-console.log(a/b);
-console.log(a*b);
+// console.log(a+b);
+// console.log(a/b);
+// console.log(a*b);
 
-console.log(10>5);
-console.log(10<5);
-console.log(10===10);
-console.log(10===5);
-console.log(10==="5");
-console.log(10 !==5);
-console.log(10 !==10);
+// console.log(10>5);
+// console.log(10<5);
+// console.log(10===10);
+// console.log(10===5);
+// console.log(10==="5");
+// console.log(10 !==5);
+// console.log(10 !==10);
 
-//logical operators
+// //logical operators
 
-let age =20;
+// let age =20;
 
-console.log(age >28 && age <60);
-console.log(age <10 || age >18);
-console.log(!(age >18));
+// console.log(age >28 && age <60);
+// console.log(age <10 || age >18);
+// console.log(!(age >18));
 
 
-let number = -5;
+// let number = -5;
 
-if (number === -5) {
-    console.log("Number is -5");
+// if (number === -5) {
+//     console.log("Number is -5");
+// }
+// else{
+//     console.log("Error");
+// }
+// // for loop
+
+// for(let i=1; i<=5; i++){
+//     console.log(i);
+// }
+// for (let i=1; i<=5; i++){
+//     let marks =i*20;
+//     if(marks>=80){
+//         console.log("Grade A" );
+//     }else if (marks >=60){
+//         console.log("Grade B");
+//     }else{
+//         console.log("Grade F");
+//     }
+// }
+
+function greet(name){
+    console.log("Hello," + name  );
 }
-else{
-    console.log("Error");
+greet(" Alice");
+function add(a ,b){
+    return a+b;
 }
-// for loop
+function sub(a,b){
+    return a-b;
+}
+let sum =add(10,5);
+let difference = sub(18,5);
+console.log(sum);
+console.log(difference);
 
-for(let i=1; i<=5; i++){
-    console.log(i);
-}
-for (let i=1; i<=5; i++){
-    let marks =i*20;
-    if(marks>=80){
-        console.log("Grade A" );
-    }else if (marks >=60){
-        console.log("Grade B");
-    }else{
-        console.log("Grade F");
-    }
-}
